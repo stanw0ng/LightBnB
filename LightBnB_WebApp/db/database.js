3c6cee18b965174ca621a58313da94c8cc12a65a
stanw0ng <user.stanley@gmail.com>
2023-04-06 16:21:27 -0400

    modified getAllReservations to work with database

--- LightBnB_WebApp/db/database.js
+++ LightBnB_WebApp/db/database.js
@@ -64,9 +64,22 @@ const addUser = function(user) {
  * @param {string} guest_id The id of the user.
  * @return {Promise<[{}]>} A promise to the reservations.
  */
-const getAllReservations = function (guest_id, limit = 10) {
-  return getAllProperties(null, 2);
-};
+
+const getAllReservations = function(guest_id, limit = 10) {
+  return pool.query(`
+  SELECT reservations.*, properties.*, AVG(property_reviews.rating) as average_rating
+  FROM reservations
+	JOIN properties ON reservations.property_id = properties.id
+	JOIN property_reviews ON properties.id = property_reviews.property_id
+	JOIN users ON reservations.guest_id = users.id
+  WHERE users.id = $1
+  GROUP BY reservations.id, properties.id
+  ORDER BY reservations.start_date
+  LIMIT $2;
+  `, [guest_id, limit])
+    .then(res => res.rows)
+    .catch((err => console.error('query error', err.stack)));
+}
 
 /// Properties
 
