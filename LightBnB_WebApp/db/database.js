899d80e011a29d3d909f0f5ba8e393405ec21e0d
stanw0ng <user.stanley@gmail.com>
2023-04-06 15:24:59 -0400

    refactored addUser

--- LightBnB_WebApp/db/database.js
+++ LightBnB_WebApp/db/database.js
@@ -36,7 +36,7 @@ const getUserWithEmail = function(email) {
 const getUserWithId = function(id) {
   return pool.query(`
   SELECT * FROM users
-  WHERE id = $1
+  WHERE id = $1;
   `, [id])
   .then(res => res.rows[0])
   .catch((err => console.error('query error', err.stack)));
@@ -47,12 +47,15 @@ const getUserWithId = function(id) {
  * @param {{name: string, password: string, email: string}} user
  * @return {Promise<{}>} A promise to the user.
  */
-const addUser = function (user) {
-  const userId = Object.keys(users).length + 1;
-  user.id = userId;
-  users[userId] = user;
-  return Promise.resolve(user);
-};
+
+const addUser = function(user) {
+  return pool.query(`
+  INSERT INTO users (name, email, password)
+  VALUES ($1, $2, $3) RETURNING *;
+  `, [user.name, user.email, user.password])
+  .then(res => res.rows[0])
+  .catch((err => console.error('query error', err.stack)));
+}
 
 /// Reservations
 
